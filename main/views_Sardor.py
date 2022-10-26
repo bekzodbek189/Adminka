714e404b56c15939ab0f09fb3144078627ff4615
Firstname Lastname <ssirojiddinov777@gmail.com>
2022-10-26 09:59:59 +0500

    somth commit

--- main/views_Sardor.py
+++ main/views_Sardor.py
@@ -1,6 +1,21 @@
 from django.shortcuts import redirect, render
 from .models import *
+from django.contrib.auth.decorators import login_required
 
 
 def information(request):
-    return render(request, 'information.html')
+    info = Information.objects.last()
+    return render(request, 'information.html', {'info': info})
+
+
+
+@login_required(login_url="sign_in")
+def Add_reklama(request):
+    user = request.user
+    if user.status == 1:
+        logo = request.FILES['img']
+        company_name = request.POST['company_name']
+        description = request.POST['description']
+        Information.objects.create(company_name=company_name, logo=logo, description=description)
+        return redirect('reklama')
+    return redirect('sign-in')
