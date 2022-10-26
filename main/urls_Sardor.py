050f623907f4d08fd424278c182d0da68c00f830
Firstname Lastname <ssirojiddinov777@gmail.com>
2022-10-26 20:02:18 +0500

    information

--- main/urls_Sardor.py
+++ main/urls_Sardor.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views_Sardor import *
 
 urlpatterns = [
-    path('information/', information, name='information')
+    path('information/', information, name='information'),
+    path('add_info/', Add_info, name='add_info'),
+    path('change_info/<str:pk>/', change_info, name='change_info')
 
 ]
--- main/views_Sardor.py
+++ main/views_Sardor.py
@@ -10,12 +10,41 @@ def information(request):
 
 
 @login_required(login_url="sign_in")
-def Add_reklama(request):
+def Add_info(request):
     user = request.user
     if user.status == 1:
         logo = request.FILES['img']
         company_name = request.POST['company_name']
         description = request.POST['description']
         Information.objects.create(company_name=company_name, logo=logo, description=description)
-        return redirect('reklama')
+        return redirect('information')
+    return redirect('sign-in')
+
+
+
+
+def change_info(request, pk):
+    user = request.user
+    info = Information.objects.get(id=pk)
+
+    if user.status == 1:
+        logo = request.FILES['img']
+        company_name = request.POST['company_name']
+        description = request.POST['description']
+        info.logo = logo
+        info.company_name = company_name
+        info.description = description
+        info.save()
+        return redirect("information")
+    return render(request, 'change_information.html')
+
+
+@login_required(login_url="sign_in")
+def Delete_Reklama(request, pk):
+    user = request.user
+    if user.status == 1:
+        info = Information.objects.get(id=pk)
+        info.status = 2
+        info.save()
+        return redirect('information')
     return redirect('sign-in')
