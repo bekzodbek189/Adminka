ba31105c45c022117412f158047973a489d79488
Firstname Lastname <ssirojiddinov777@gmail.com>
2022-11-02 10:51:46 +0500

    completed1

--- api/urls.py
+++ api/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('ads/', AdsView.as_view()),
     path('category/', CategoryView.as_view()),
     path('subcategory/', SubcategoryView.as_view()),
+    path('about/', AboutView.as_view({'get':'list'})),
+    path('reklama/', ReklamaView.as_view({'get':'list'})),
+    path('info/', InformationView.as_view({'get':'list'})),
 ]
--- olx/urls.py
+++ olx/urls.py
@@ -17,12 +17,29 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+scheme_view = get_schema_view(
+    openapi.Info(
+        title='Cyber Api',
+        default_version='v1',
+        description='best of the best',
+
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny]
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('main.urls')),
     path("api/", include('api.urls')),
     path("", include('main.urls_Abdulbosit')),
+    path('swagger/ui/', scheme_view.with_ui('swagger', cache_timeout=0), name='scheme-swagger-ui')
+
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
